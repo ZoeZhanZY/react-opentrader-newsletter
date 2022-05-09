72b291cbe37fa9170bf02def6f52350be245f2a6
ZoeZhanZY <zoe.zhanzy@gmail.com>
2022-05-09 16:27:07 +1000

    rwwrote the userList data structure

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,15 @@ import PaginationComponent from "./components/PaginationComponent/PaginationComp
 import Button from "@mui/material/Button";
 
 function App() {
-  const [userLists, setUserLists] = useState([]);
+  const [userLists, setUserLists] = useState({});
+  const [listOrders, setListOrders] = useState({});
   const [renderedList, setRenderedList] = useState([]);
   const [adLink, setAdLink] = useState();
   const [pageCount, setPageCount] = useState();
   const [currentPage, setCurrentPage] = useState(1);
+  const currentPageNumberString = currentPage.toString();
 
-  // Fetch data from backend
+  //============new version==============
   useEffect(() => {
     const fetchData = async () => {
       const res = await fetch(
@@ -20,60 +22,113 @@ function App() {
       );
       const { page, data, total_pages, support } = await res.json();
       const slicedData = data.slice(0, 5);
-      const newUserList = { page, slicedData };
+      const pageNumber = page.toString();
+      const initialOrder = slicedData.map((i) => i.id);
 
-      const isCurrentPageDataExisted = userLists.some(
-        (pageObject) => pageObject.page === currentPage
-      );
+      const newUserList = {
+        [pageNumber]: {
+          slicedData: [...slicedData],
+        },
+      };
 
-      if (!isCurrentPageDataExisted) {
-        setUserLists([...userLists, newUserList]);
+      const newListOrder = {
+        [pageNumber]: {
+          order: [...initialOrder],
+        },
+      };
+
+      if (!(currentPage in userLists)) {
+        setListOrders({ ...listOrders, ...newListOrder });
       }
 
+      setUserLists({ ...userLists, ...newUserList });
       setPageCount(total_pages);
       setAdLink(support.url);
     };
-
     fetchData();
   }, [currentPage]);
 
-  // Update rendered list data
+
   useEffect(() => {
-    console.log("userLists", userLists);
-
-    const renderedData =
-      userLists.length > 0 &&
-      userLists.find((el) => el.page === currentPage)?.slicedData;
-
+    console.log({ userLists });
+    const targetData = userLists[currentPageNumberString]?.slicedData;
+		const targetOrder = listOrders[currentPageNumberString]?.order;
+		
+    console.log({ targetData });
+    const renderedData = targetOrder?.map((order) => {
+      return targetData?.find((user) => user.id === order);
+    });
+    console.log("renderedData", renderedData);
     setRenderedList(renderedData);
-  }, [userLists, currentPage]);
+  }, [userLists, listOrders, currentPage]);
+
+  // ===========old version==============
+  // // Fetch data from backend
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const res = await fetch(
+  //       "https://reqres.in/api/users?page=" + currentPage
+  //     );
+  //     const { page, data, total_pages, support } = await res.json();
+  //     const slicedData = data.slice(0, 5);
+  //     const newUserList = { page, slicedData };
+
+  //     const isCurrentPageDataExisted = userLists.some(
+  //       (pageObject) => pageObject.page === currentPage
+  //     );
+
+  //     if (!isCurrentPageDataExisted) {
+  //       setUserLists([...userLists, newUserList]);
+  //     }
+
+  //     setPageCount(total_pages);
+  //     setAdLink(support.url);
+  //   };
+
+  //   fetchData();
+  // }, [currentPage]);
+
+  // // Update rendered list data
+  // useEffect(() => {
+  //   console.log("userLists", userLists);
+
+  //   const renderedData =
+  //     userLists.length > 0 &&
+  //     userLists.find((el) => el.page === currentPage)?.slicedData;
+
+  //   setRenderedList(renderedData);
+  // }, [userLists, currentPage]);
 
   const handleShuffle = () => {
     const randomShuffle = (a, b) => {
       return 0.5 - Math.random();
     };
-    const targetUserList = userLists.find(
-      (el) => el.page === currentPage
-    ).slicedData;
-    const shuffledList = [...targetUserList.sort(randomShuffle)];
 
-    setRenderedList(shuffledList);
+    const targetOrder = listOrders[currentPageNumberString]?.order;
+    const shuffledOrder = [...targetOrder.sort(randomShuffle)];
+
+    console.log({ shuffledOrder });
+
+    const newListOrders = { ...listOrders };
+    newListOrders[currentPageNumberString].order = shuffledOrder;
+    setListOrders(newListOrders);
   };
-
   const handleReset = () => {
     const compareNumber = (a, b) => {
-      return a.id - b.id;
+      return a - b;
     };
 
-    const targetUserList = userLists.find(
-      (el) => el.page === currentPage
-    ).slicedData;
-    const resetList = [...targetUserList.sort(compareNumber)];
-    setRenderedList(resetList);
+    const targetOrder = listOrders[currentPageNumberString]?.order;
+    const resetOrder = [...targetOrder.sort(compareNumber)];
+    console.log({ resetOrder });
+
+    const newListOrders = { ...listOrders };
+    newListOrders[currentPageNumberString].order = resetOrder;
+    setListOrders(newListOrders);
   };
 
   return (
-    <div className="container" style={{ color: "white" }}>
+    <div className="container">
       <div className="hero">
         <Hero adLink={adLink} />
       </div>
